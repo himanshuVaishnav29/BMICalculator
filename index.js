ac6e25076600c9cdef856dce713f909ba035a446
himanshu1268 <himanshu.29vaishnav@gmail.com>
2024-04-02 15:15:23 +0530

    initial setup bmi calculator

---
+++ index.js
@@ -0,0 +1,52 @@
+const express=require("express");
+const fs=require("fs");
+const userData=require("./userData.json");
+const app=express();
+
+app.use(express.urlencoded({extended:'false'}));
+app.use(express.static("./"));
+
+app.get("/",(req,res)=>{
+    // res.send("home");
+    fs.readFile("index.html","utf8",(err,data)=>{
+        if(err)console.log("Error fetching index.html",err);
+        else
+        // res.setHeader({'content-type':'text/html'});
+        res.send(data);
+    })
+})
+// fs.readFile("index.html","")
+
+
+app.post("/saveData",(req,res)=>{
+    const body=req.body;
+    const bmi=(body.weight/(body.height*body.height)).toFixed(3);
+    userData.push({
+        userName:body.userName,
+        age:body.age,
+        gender:body.gender,
+        weight:body.weight,
+        height:body.height,
+        id:userData.length+1,
+        bmi:bmi
+    });
+    fs.writeFile("userData.json",JSON.stringify(userData,2),(err)=>{
+        if(err){
+            console.log("Error saving the data in json")
+        }
+    })
+    res.redirect("/");
+    res.json({msg:'Saved data successfully'});
+});
+
+app.get("/getUserData",(req,res)=>{
+    const allUserData=userData;
+    res.json(allUserData);
+})
+
+
+
+app.listen(3500,(err)=>{
+    if(err)console.log("Error starting server");
+    else console.log("Server listening at port 3500");
+})
